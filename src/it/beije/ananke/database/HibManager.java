3c6cbce8705430f7aba2e330c8ef15f2a614d119
SaraDeMonaco <demonacosara@gmail.com>
2021-02-02 17:54:19 +0100

    hibernate manager

--- src/it/beije/ananke/database/HibManager.java
+++ src/it/beije/ananke/database/HibManager.java
@@ -2,7 +2,9 @@ package it.beije.ananke.database;
 
 import it.beije.ananke.file.Contatto;
 import it.beije.ananke.file.Rubrica;
+import it.beije.ananke.Singleton;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -16,17 +18,15 @@ import org.hibernate.query.Query;
 public class HibManager {
 	public Rubrica r = new Rubrica();
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		HibManager hib = new HibManager();
-		Configuration configuration = new Configuration().configure();
-		SessionFactory sf = configuration.buildSessionFactory();
 		
 		menu();
 		Scanner s = new Scanner(System.in);
 		String st = s.next();
 
 		while (!st.equalsIgnoreCase("5")) {
-			hib.choice(st, s, sf);
+			hib.choice(st, s);
 			menu();
 			st = s.next();
 		}
@@ -34,7 +34,6 @@ public class HibManager {
 		
 		System.out.println("Arrivederci!");
 		s.close();
-		sf.close();
 	
 	}
 	
@@ -51,29 +50,26 @@ public class HibManager {
 	}
 	
 	
-	public void choice(String i, Scanner s, SessionFactory sf) {
-		Session session = sf.openSession();
-		
+	public void choice(String i, Scanner s) throws IOException {
+		Session session = HybernateSessionManager.getSession();
 		switch(i) {
-			case "0": stampa(sf); break;
-			case "1": break;
-			case "2": break;
-			case "3": break;
-			case "4": break;
+			case "0": lettura(session); break;
+			case "1": inserisci(session, s); break;
+			case "2": modifica(session, s); break;
+			case "3": eliminazione(session, s); break;
+			case "4": ricerca(session, s); break;
 			case "5": break;
 			case "6": break;
 			default: System.out.println("L'opzione selezionata non è valida.");
 		}
-		
 		session.close();
 	}
 	
 	
-	public void stampa(SessionFactory sf) {
-		List<Contatto> contatti = lettura(sf);
+	public void stampa(List<Contatto> contatti) {
 		if(contatti.size() != 0) {
 			for(Contatto c : contatti) {
-				c.toString();
+				System.out.println(c.toString());
 			}
 		}else {
 			System.out.println("La rubrica è vuota.");
@@ -81,19 +77,19 @@ public class HibManager {
 	}
 	
 	
-	public List<Contatto> lettura(SessionFactory sf){
-		Session session = sf.openSession();
+	public List<Contatto> lettura(Session session){
+		List<Contatto> contatti = new ArrayList<>();
 		String hqlSelect = "SELECT c FROM Contatto as c";
 		Query<Contatto> query = session.createQuery(hqlSelect);
-		List<Contatto> contatti = query.list();
+		contatti = query.list();
+		stampa(contatti);
 		return contatti;
 	}
 	
 	
-	public List<Contatto> scan(SessionFactory sf, Scanner s){
-		Session session = sf.openSession();
+	public List<Contatto> scan(Session session, Scanner s){
 		System.out.println("Scegliere in quale campo cercare:\n"
-				+ "0: ID\n 1: NOME\n2: COGNOME\n3: EMAIL\n4: TELEFONO\n"
+				+ "0: ID\n1: NOME\n2: COGNOME\n3: EMAIL\n4: TELEFONO\n"
 				+ "(Attenzione il numero di telefono deve contenere eventuali prefissi)");
 		String str = s.next();
 		List<Contatto> contatti = new ArrayList<>();
@@ -130,12 +126,83 @@ public class HibManager {
 	}
 	
 	
+	public void ricerca(Session session, Scanner s) {
+		List<Contatto> contatti = scan(session, s);
+		if(contatti.size() == 0) {
+			System.out.println("Non ci sono elementi che corrispondono ai criteri di ricerca.");
+		}else {
+			stampa(contatti);
+		}
+	}
 	
 	
+	public void inserisci(Session session, Scanner s) throws IOException {
+		Transaction transaction = session.beginTransaction();
+		Contatto contatto = r.defContatto(s);
+		session.save(contatto);
+		transaction.commit();
+		System.out.println("La voce è stata inserita correttamente.");
+	}
 	
 	
+	public void eliminazione(Session session, Scanner s) {
+		List<Contatto> contatti = scan(session, s);
+		if(contatti.size() == 0) {
+			System.out.println("Non ci sono elementi che corrispondono ai criteri di ricerca.");
+		}else {
+			for(Contatto c : contatti) {
+				Transaction transaction = session.beginTransaction();
+				session.delete(c);
+				transaction.commit();
+				System.out.println("I campi selezionai sono stati eliminati.");
+			}
+		}
+	}
 	
 	
+	public void modifica(Session session, Scanner s) {
+		List<Contatto> contatti = scan(session, s);
+		if(contatti.size() == 0) {
+			System.out.println("Non ci sono elementi che corrispondono ai criteri di ricerca.");
+		}else {
+			stampa(contatti);
+			System.out.println("Scegliere il campo da modificare: \n"
+					+ "1: NOME\n2: COGNOME\n3: EMAIL\n4: TELEFONO\n"
+					+ "(Attenzione il numero di telefono deve contenere eventuali prefissi)");
+			String str = s.next();
+			
+			try {
+				int i = Integer.parseInt(str);
+				while(i <= 0 || i > 4) {
+					System.out.println("Scelta non valida. Inserire un nuovo valore.");
+					str = s.next();
+					i = Integer.parseInt(str);
+				}
+				
+				System.out.println("Inserire il nuovo valore da salvare: ");
+				String nuovo = s.next();
+				
+				for(Contatto c : contatti) {
+					Transaction transaction = session.beginTransaction();
+					switch(i) {
+						case 1: c.setName(nuovo); break;
+						case 2: c.setSurname(nuovo); break;
+						case 3: c.setEmail(nuovo); break;
+						case 4: c.setTelephone(nuovo); break;
+					}	
+					session.save(c);
+					transaction.commit();
+					System.out.println("Le modifiche sono state salvate.");
+				}
+				
+			}catch(NumberFormatException e) {
+				e.printStackTrace();
+				System.out.println("Il valore inserito non è un numero. Le modifiche non saranno salvate.");
+			}
+			
+			
+		}
+	}
 	
 	
 	
--- src/it/beije/ananke/database/HybernateSessionManager.java
+++ src/it/beije/ananke/database/HybernateSessionManager.java
@@ -4,7 +4,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import it.beije.ananke.Contatto;
+import it.beije.ananke.file.Contatto;
 
 public class HybernateSessionManager {
 	
