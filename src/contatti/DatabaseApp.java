69949fe8ecb35a56afbd4376abb5ccab084eac4d
faheemjan5000 <faheemjan500@gmail.com>
2021-02-02 09:02:02 +0100

    main app updated

---
+++ src/contatti/DatabaseApp.java
@@ -0,0 +1,47 @@
+package contatti;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseApp {
+
+	public static void main(String[] args) throws IOException, SQLException {
+		List<User> allUsers = new ArrayList<>();
+		UserManager userManager = new UsersInJdbcManager();
+		
+		
+		String email = "faheemjan58@gmail.com";
+		User user = userManager.getUser(email);
+		 if(user!=null)
+		System.out.println("User retrieved from database is : "+ user.getFirstName()+ " " + user.getEmail() );
+		 else
+			 System.out.println("user not found in database!");
+		
+//		String email = "kaka58@gmail.com";
+//		userManager.removeUser(email);
+		
+//		allUsers = userManager.getAllUsers();
+//		for(User user : allUsers) {
+//			System.out.println(user.getFirstName()+" " 
+//		+user.getLastName() + " " +user.getEmail() + " "+user.getPhoneNumber());
+//		}
+		
+//		User user = new User("kaka","macho","kaka58@gmail.com","82811111");
+//		if(userManager.setUser(user) )
+//		{
+//			System.out.println("person added successfully!");
+//			
+//		}
+//		else
+//		{
+//			System.out.println("this person already exist!");
+//		}
+		
+	}
+	
+	
+	}
+
+
