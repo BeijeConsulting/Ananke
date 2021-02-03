90d1c93dc8194c0427cff0b5fe115ccba44e652e
Padawan04 <demonacosara@gmail.com>
2021-02-03 12:10:48 +0100

    adding lost classes

---
+++ src/it/beije/ananke/database/JPAmanager.java
@@ -0,0 +1,68 @@
+package it.beije.ananke.database;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import it.beije.ananke.Contatto;
+
+
+public class JPAmanager {
+
+	public static void main(String[] args) {
+		
+//		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Rubrica");
+//		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		EntityManager entityManager = RubricaEntityManager.getEntityManager();
+		
+//		Contatto contatto1 = entityManager.find(Contatto.class, 1);
+//		System.out.println(contatto1);
+
+		//JPQL
+		String jpqlSelect = "SELECT c FROM Contatto as c";
+		Query query = entityManager.createQuery(jpqlSelect);
+		List<Contatto> contatti = query.getResultList();
+
+		for (Contatto contatto : contatti) {
+			System.out.println("id : " + contatto.getId());
+			System.out.println("name : " + contatto.getName());
+			System.out.println("surname : " + contatto.getSurname());
+			System.out.println("telephone : " + contatto.getTelephone());
+			System.out.println("email : " + contatto.getEmail());
+		}
+
+		//apro transazione
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		
+//		//UPDATE
+//		Contatto c1 = contatti.get(contatti.size()-1);
+//		c1.setTelephone("123456789");
+//		
+//		System.out.println("c1 ID PRE save  :" + c1.getId());
+//		entityManager.persist(c1);
+//		System.out.println("c1 ID POST save :" + c1.getId());
+		
+		//INSERT
+		Contatto newContatto = new Contatto();
+		newContatto.setName("Piero");
+		newContatto.setSurname("Pelù");
+		newContatto.setEmail("ppelu@beije.it");
+		
+		System.out.println("newContatto ID PRE save  :" + newContatto.getId());
+		entityManager.persist(newContatto);
+		System.out.println("newContatto ID POST save :" + newContatto.getId());
+		
+		entityTransaction.commit();
+//		entityTransaction.rollback();
+		
+		entityManager.close();
+		
+	}
+
+}
