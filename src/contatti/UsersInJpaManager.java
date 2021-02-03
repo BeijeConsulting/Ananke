1bae158b1dcd8d3d45f3ffd68936567d4f09da4a
faheemjan5000 <faheemjan500@gmail.com>
2021-02-03 17:44:41 +0100

    jpa complete with menu

---
+++ src/contatti/UsersInJpaManager.java
@@ -0,0 +1,65 @@
+package contatti;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class UsersInJpaManager implements UserManager {
+
+	
+	private final EntityManagerFactory entityManagerFactory = Persistence
+			.createEntityManagerFactory("Ananke");
+	private final EntityManager entityManager = entityManagerFactory.createEntityManager();
+	
+	@Override
+	public boolean setUser(User user) throws IOException, SQLException {
+		entityManager.getTransaction().begin();
+		entityManager.persist(user);
+		entityManager.getTransaction().commit();
+		return false;
+	}
+
+	@Override
+	public void removeUser(String email) {
+		User user= null;
+		user= entityManager.find(User.class, 2);
+		entityManager.getTransaction().begin();
+		entityManager.remove(user);
+		entityManager.getTransaction().commit();
+	}
+
+	@Override
+	public void updateUser(User user) {
+		entityManager.getTransaction().begin();
+		entityManager.merge(user);
+		entityManager.getTransaction().commit();
+	}
+
+	@Override
+	public User getUser(String email) {
+		List<User> allUsers = new ArrayList<>();
+		allUsers = getAllUsers();
+		for(User user : allUsers) {
+			if(user.getEmail().equals(email)) {
+				return user;
+			}
+		}
+		return null;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<User> getAllUsers() {
+		List<User> allUsers = new ArrayList<>();
+		entityManager.getTransaction().begin();
+		allUsers = entityManager.createQuery("SELECT u FROM User u").getResultList();
+		entityManager.getTransaction().commit();
+		return allUsers;
+	}
+
+}
