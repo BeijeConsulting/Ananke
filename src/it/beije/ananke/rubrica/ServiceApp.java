ddcf76325349a8f2329de1889cc0dadaeec068bf
BitettiF <bitetti.f@gmail.com>
2021-02-05 09:30:49 +0100

    Update ServiceApp.java

--- src/it/beije/ananke/rubrica/ServiceApp.java
+++ src/it/beije/ananke/rubrica/ServiceApp.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -16,6 +17,7 @@ public class ServiceApp {
 	Connection conn = null;
 	PreparedStatement preparedStatement = null;
 	Statement statement = null;
+	ResultSet rs = null;
 
 	public ArrayList<Contatto> CaricaFileCsv(String path, String nomeFile) throws IOException {	
 		ArrayList<Contatto> listContatti = new ArrayList<Contatto>();
@@ -39,25 +41,57 @@ public class ServiceApp {
 
 		return listContatti;
 	}
+	
+	
+	public boolean isDoppio(ArrayList<String> lista, String email, int index) {
+		if(index == lista.size()) {
+			return false;
+		}
+		if(lista.get(index).equals(email)) {
+			return true;
+		}
+		return isDoppio(lista,email,++index);
+	}
 
 
 
 	public void aggiornaContattiDB(ArrayList<Contatto>contatto) throws SQLException {
+		
+		ArrayList<String> emaillista = new ArrayList<>();
 		ConnectionDb con = new ConnectionDb();
 		this.conn = con.connectionJDBC(); 
 		String sql = "INSERT INTO contatti (name, surname,email,telephone) VALUES (?,?,?,?)";
+		String sql1 = "SELECT email FROM contatti";
 	    preparedStatement = conn.prepareStatement(sql);
+	   
+	    rs = statement.executeQuery(sql1);
+		while(rs.next()) {
+			String email = rs.getString("email");
+			emaillista.add(email);
+			
+		}
+		
 	    
 		for(Contatto c : contatto) {
-			preparedStatement.setString(1, c.getName());
-			preparedStatement.setString(2, c.getSurname());
-			preparedStatement.setString(3, c.getTelephone());
-			preparedStatement.setString(4, c.getEmail());
+			if(!isDoppio(emaillista,c.getEmail(),0)) {
+			String name = c.getName();
+			String surname = c.getSurname();
+			String telephone = c.getTelephone();
+			String email = c.getEmail();
+			preparedStatement.setString(1, name);
+			preparedStatement.setString(2, surname);
+			preparedStatement.setString(3, email);
+			preparedStatement.setString(4, telephone);
   	        preparedStatement.execute();
+			} else {
+				System.out.println("l'email "+ c.getEmail()+ " è doppione, non inserita");
+			}
 		}
 		
 		conn.close();
 		preparedStatement.close();
+		rs.close();
+		statement.close();
 	
 		
 
